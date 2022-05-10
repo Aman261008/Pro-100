3972e2f2e65f743e905da2b0f69e20c7b728220a
Aman261008 <79710138+Aman261008@users.noreply.github.com>
2022-05-10 23:30:56 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,16 @@
+class atm(object):
+    def __init__(self,atmCardNumber,pinNumber):
+        self.atmCardNumber=atmCardNumber
+        self.pinNumber=pinNumber
+        
+        def cashWithdrawl(self):
+            print("cashWithdrawled")
+            
+        def balanceEnquiry(self):
+            print("balanceEnquiried")
+
+        def noCash(self):
+            print("There is no Cash...")
+
+        def miniStatment(self):
+            print("miniStatment in progress..")
